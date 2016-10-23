7779ce1ef30d0d508b1a5c84f0512cf1377b1555
liuhuanzhu <fz0207@126.com>
2016-10-23 11:34:10 +0800

    Signed-off-by: liuhuanzhu <fz0207@126.com>

--- easy-share/app.js
+++ easy-share/app.js
@@ -10,6 +10,9 @@ var users = require('./routes/users');
 
 var app = express();
 
+//生成版本
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
